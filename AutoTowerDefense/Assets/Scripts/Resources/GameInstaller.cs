758aab688d77f1b3e90b1adaa41e73e335fec68c
Michael Dumfart <michi.dumfart@gmail.com>
2022-01-09 15:41:27 +0100

    add drawer animation

--- AutoTowerDefense/Assets/Scripts/Resources/GameInstaller.cs
+++ AutoTowerDefense/Assets/Scripts/Resources/GameInstaller.cs
@@ -1,4 +1,5 @@
 using Core.GameManager;
+using Core.UI;
 using UnityEngine;
 using Zenject;
 
@@ -9,6 +10,7 @@ namespace Core.Resources
         public override void InstallBindings()
         {
             Container.Bind<ILocalGameManager>().To<LocalGameManager>().AsSingle();
+            Container.Bind<IUiController>().To<UiController>().AsSingle();
         }
     }
 }
