82d84ca927317a03e51ee0cb6239626a76364087
Michael Dumfart <michi.dumfart@gmail.com>
2022-01-25 22:02:40 +0100

    add addTurret to repo

--- AutoTowerDefense/Assets/Scripts/Core/Game/TurretRepository.cs
+++ AutoTowerDefense/Assets/Scripts/Core/Game/TurretRepository.cs
@@ -5,20 +5,41 @@ using UnityEngine;
 
 namespace Core.Game
 {
-    public class TurretRepository
+    public class TurretRepository: MonoBehaviour
     {
         private List<GameObject> tier1 = new List<GameObject>();
         private List<GameObject> tier2 = new List<GameObject>();
         private List<GameObject> tier3 = new List<GameObject>();
+        private const int MaxNumberOfSameTurret = 2;
 
         public void AddTurret(GameObject go)
         {
             var turretBase = go.GetComponent<TurretBase>();
 
             if (turretBase.tier != TurretTier.Tier1) return;
-            
-            // tier1.Find(t => t.GetComponent<TurretBase>())            
-            
+
+            var t1Siblings = tier1.FindAll(t => t.GetComponent<TurretBase>().type == turretBase.type);
+
+            if (t1Siblings.Count == 2)
+            {
+                t1Siblings.ForEach(Destroy);
+                turretBase.tier++;
+                
+                
+                
+                
+                
+                
+                
+                var t2Siblings = tier2.FindAll(t => t.GetComponent<TurretBase>().type == turretBase.type);
+
+                if (t2Siblings.Count == 2)
+                {
+                    t2Siblings.ForEach(Destroy);
+                    turretBase.tier++;
+                    tier2.Remove()
+                }
+            }
         }
     }
 }
