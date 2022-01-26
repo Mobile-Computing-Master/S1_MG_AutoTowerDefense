412b4e79468cfd3bddb39bc2a2a8e067c1d70df8
Michael Dumfart <michi.dumfart@gmail.com>
2022-01-26 13:22:15 +0100

    add health bar base

---
+++ AutoTowerDefense/Assets/Scripts/Core/UI/Components/HealthBar.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Core.UI.Components
+{
+    public class HealthBar : MonoBehaviour
+    {
+        public GameObject fullHeartPrefab;
+        public GameObject halfHeartPrefab;
+        private List<GameObject> hearts = new List<GameObject>();
+        
+        private HealthService _healthService;
+        
+        private void OnEnable()
+        {
+            Initiate();
+        }
+
+        private void UpdateHealthBar()
+        {
+            int health = 20;
+
+            if (health % 2 == 0)
+            {
+                SpawnHearts(health);
+            }
+            else
+            {
+                SpawnHearts(health - 1);
+            }
+        }
+
+        private void SpawnHearts(int health)
+        {
+            
+        }
+
+        private void Initiate()
+        {
+            _healthService = GameObject.Find("Context").GetComponent<HealthService>();
+        }
+    }
+}
