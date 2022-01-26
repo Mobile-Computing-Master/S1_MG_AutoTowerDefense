4d4ce13a54e6aa5d63966b538f8a4762dca2d6fd
Mario Schober <30479987+V3RO@users.noreply.github.com>
2022-01-26 13:23:07 +0100

    implemented HealthService

--- AutoTowerDefense/Assets/Scripts/Core/Game/RoundService.cs
+++ AutoTowerDefense/Assets/Scripts/Core/Game/RoundService.cs
@@ -11,17 +11,20 @@ namespace Core.Game
         
         private CreepSpawner _spawner;
         private float _roundCooldown = 0f;
+        private HealthService _healthService;
+        private bool _playerDied = false;
 
         private void Start()
         {
             _spawner = GameObject.Find("Spawner").GetComponent<CreepSpawner>();
+            _healthService = GameObject.Find("Context").GetComponent<HealthService>();
             _spawner.FillCreepQueue();
+            _healthService.OnPlayerDied += OnPlayerDied;
         }
-
 
         private void Update()
         {
-            if (!_spawner.IsEverythingSpawned()) return;
+            if (!_spawner.IsEverythingSpawned() || _playerDied) return;
             var creeps = FindObjectsOfType<CreepBase>();
             if (creeps.Length > 0) return;
 
@@ -31,6 +34,11 @@ namespace Core.Game
             _spawner.roundSize = (int)(_spawner.roundSize * creepIncreaseMultiplier);
             _spawner.FillCreepQueue();
             _roundCooldown = 0;
+        }
+
+        private void OnPlayerDied()
+        {
+            _playerDied = true;
         }
     }
 }
