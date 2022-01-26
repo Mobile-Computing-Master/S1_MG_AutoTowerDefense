632294f186ecb3524be59d837df5b86a7058e4b4
Michael Dumfart <michi.dumfart@gmail.com>
2022-01-26 18:52:31 +0100

    add menu and help ui

--- AutoTowerDefense/Assets/Scripts/Menu/InGameMenu.cs
+++ AutoTowerDefense/Assets/Scripts/Menu/InGameMenu.cs
@@ -1,18 +1,39 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
-public class InGameMenu : MonoBehaviour
+namespace Menu
 {
-    // Start is called before the first frame update
-    void Start()
+    public class InGameMenu : MonoBehaviour
     {
-        
-    }
+        private GameObject _helpWrapper;
+        private GameObject _helpButton;
+        private GameObject _background;
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+        private void Start()
+        {
+            Initiate();
+        }
+
+        public void ShowHelp()
+        {
+            _helpWrapper.SetActive(true);
+            _helpButton.SetActive(false);
+            _background.SetActive(true);
+        }
+
+        public void HideHelp()
+        {
+            _helpWrapper.SetActive(false);
+            _helpButton.SetActive(true);
+            _background.SetActive(false);
+        }
+
+        private void Initiate()
+        {
+            _helpWrapper = GameObject.Find("HelpWrapper");
+            _helpButton = GameObject.Find("HelpButton");
+            _background = GameObject.Find("Background");
+            
+            HideHelp();
+        }
     }
 }
--- AutoTowerDefense/Assets/Scripts/Menu/MainMenu.cs
+++ AutoTowerDefense/Assets/Scripts/Menu/MainMenu.cs
@@ -1,18 +1,47 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-public class MainMenu : MonoBehaviour
+namespace Menu
 {
-    public void StartMap(int mapNumber)
+    public class MainMenu : MonoBehaviour
     {
-        // TODO: Add other maps
-        SceneManager.LoadScene("Scenes/main");
-    }
+        private GameObject _helpWrapper;
+        private GameObject _mainWrapper;
 
-    public void Exit()
-    {
-        Application.Quit();
+        private void Start()
+        {
+            Initiate();
+        }
+
+        public void StartMap(int mapNumber)
+        {
+            // TODO: Add other maps
+            SceneManager.LoadScene("Scenes/main");
+        }
+
+        public void Exit()
+        {
+            Application.Quit();
+        }
+
+        public void ShowHints()
+        {
+            _helpWrapper.SetActive(true);
+            _mainWrapper.SetActive(false);
+        }
+
+        public void HideHints()
+        {
+            _helpWrapper.SetActive(false);
+            _mainWrapper.SetActive(true);
+        }
+    
+        private void Initiate()
+        {
+            _helpWrapper = GameObject.Find("HelpWrapper");
+            _mainWrapper = GameObject.Find("MainWrapper");
+        
+            _helpWrapper.SetActive(false);
+        }
     }
 }
